e686d844b45ee77b18ccff28f233f5c97bae7f26
Yahor Simak <simak_yahor@mail.ru>
2018-01-10 23:44:04 +0300

    [wip] 3/4 test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-module.exports = function isPointsOnLine(points) {
-  // your solution here
+module.exports = function isPointsOnLine(points) { 
+  return points.every(([x, y]) => (x - points[0][0]) / (points[1][0] - points[0][0]) === (y - points[0][1]) / (points[1][1] - points[0][1]));
 }
